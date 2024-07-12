04c54ece12262acf500484a78b92297e5d71d602
beast3012 <53287928+beast3012@users.noreply.github.com>
2024-07-12 14:04:08 +0700

    code 12/7/2024

--- src/Componenets/Header.js
+++ src/Componenets/Header.js
@@ -1,21 +1,22 @@
 import React from 'react';
 import '../Styles/Header.css';
+import NewspaperIcon from '../Assets/newspaper.png';
 
 const Header = () => {
   return (
       <header className="header">
       <div></div>
       <div className="button-container">
-        <button className="button">
-          <i className="fas fa-video icon"></i> Video
+        <button className="video button">
+          <i class="fas fa-play-circle icon"></i> Video
         </button>
-        <button className="button">
-          <i className="fas fa-book icon"></i> eMagazine
+        <button className="eMagazine button">
+          <i src={NewspaperIcon} alt="Icon"></i> eMagazine
         </button>
-        <button className="button">
+        <button className="facebook button">
           <i className="fab fa-facebook-f" style={{ color: 'white' }}></i>
         </button>
-        <button className="button">
+        <button className="search button">
           <i className="fas fa-search" style={{ color: 'white' }}></i>
         </button>
       </div>
